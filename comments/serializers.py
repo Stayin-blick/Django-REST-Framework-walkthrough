7864f6e247cc2ac35c9ec92df335b802ff960ba8
Stayin_blick <azeezbello75@gmail.com>
2023-08-02 17:25:12 +0000

    CommentList and CommentDetail generic views

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Comment
 
 
-class CommentsSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -13,8 +13,12 @@ class CommentsSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Comments
+        model = Comment
         fields = [
            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'post',
            'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
