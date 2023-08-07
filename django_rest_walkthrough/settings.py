d725305eec2976455676227bbfc925c6c7370ff2
Stayin_blick <azeezbello75@gmail.com>
2023-08-07 17:14:03 +0000

    fix

--- django_rest_walkthrough/settings.py
+++ django_rest_walkthrough/settings.py
@@ -120,6 +120,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'django_rest_walkthrough.urls'
 
 TEMPLATES = [
